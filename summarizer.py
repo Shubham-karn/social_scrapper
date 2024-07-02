0d59a991af8193acc149e55ed60f6fd6575402fe
Shubham <shubhamkarn164@gmail.com>
2024-07-02 21:13:17 +0545

    added summary

---
+++ summarizer.py
@@ -0,0 +1,16 @@
+from transformers import pipeline
+
+summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+
+async def summary(article):
+    try:
+        summary = summarizer(article, max_length=1000, min_length=30, do_sample=False)
+        return {
+            "status": "success",
+            "summary": summary[0]['summary_text']
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
